f5ef2d2630f80865fed385e6403e1aeea46328b6
Andrew Scott <scottmeup@users.noreply.github.com>
2020-07-03 14:23:51 +1000

    Modify lrCostFunction.m
    passes tests, yet does not produce the provided expected result for cost value

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,14 +36,44 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+%Compute values for the negative and the positive case of y
+J_for_y_eq_1 = (-1.*y) .* ( log(sigmoid (X * theta) ) );
+J_for_y_eq_0 = (1-y) .* log( 1 - (sigmoid (X * theta) ) );
+
+%Subtract the results of the negative cases from the positive cases to
+%produce our cost vector for each case
+J = J_for_y_eq_1 - J_for_y_eq_0;
+
+%Create a constant for regularisation, taking lambda/m and theta as
+%the arguments
+c_regular = sum(theta.^2);
+c_regular = c_regular .* (lambda/(2.*m)); 
+
+%Apply regularisation vector to our cost vector
+%BUT! Only to the 2nd and subsequent elements
+J(2:end) = J(2:end)+c_regular;
+
+% sum our cost vector J,then divide by number of cases to get the mean value
+J = sum(J) ./ m;
 
 
 
 
 
+%Calculate the gradient
+grad = ( (sigmoid(X*theta)-y)'*X ./m );
 
 
+%Build regularisation vector for gradient: \frac{\lambda}{m}\theta_j, 
+%applies to elements 2-m, so not to apply the regularisation to the bias 
+%element
+grad_regularisation = theta';
+grad_regularisation(1) = 0;
+grad_regularisation = grad_regularisation .* (lambda./m);
 
+
+%Add regularisation vector to gradient vector
+grad = grad + grad_regularisation;
 
 % =============================================================
 
