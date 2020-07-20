651e42b3f9ed774c6cfecfe9c70ef2987d9f6880
Andrew Scott <scottmeup@users.noreply.github.com>
2020-07-20 18:17:57 +1000

    Modify linearRegCostFunction.m
    Cost function & regularization passing test case.

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -19,11 +19,24 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+%{
+Cost is defined as:
+$$J\theta =\dfrac {1}{2m}\left( \sum ^{m}_{i=1}\left( h_{\theta }\left( 
+x^{\left( i\right) }\right) -y^{\left( i\right) }\right) ^{2} \right) 
++\dfrac {\lambda }{2m}\left( \sum ^{n}_{j=1}\theta ^{2}j\right)$$
 
+Where the 2nd sum is only applied to the second and following elements of
+theta
+%}
 
+%Calculate cost
+J = (1/(2*m)).* sum( ( (X * theta) - y) .^2 );
 
-
-
+%Calculate & apply regularization for cost
+if lambda>0
+    costReg = (lambda/(2*m)) .* sum(theta(2:end).^2);
+    J = J + costReg;
+end
 
 
 
