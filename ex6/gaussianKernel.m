771984beb04a9101f1b24b1fe5130d43e3915e01
Andrew Scott <scottmeup@users.noreply.github.com>
2020-08-05 15:45:08 +1000

    Modify gaussianKernel.m
    Passing test cases.

--- ex6/gaussianKernel.m
+++ ex6/gaussianKernel.m
@@ -6,9 +6,6 @@ function sim = gaussianKernel(x1, x2, sigma)
 % Ensure that x1 and x2 are column vectors
 x1 = x1(:); x2 = x2(:);
 
-% You need to return the following variables correctly.
-sim = 0;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
 %               and x2 computed using a Gaussian kernel with bandwidth
@@ -16,7 +13,8 @@ sim = 0;
 %
 %
 
-
+%Gaussian Kernal:
+sim = exp( sum( (x1-x2).^2 ) .* (-1/(2*(sigma).^2)) );
 
 
 
