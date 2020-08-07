8b7bdb6406e1d9e769eb28ddb56a16c17fe436f8
Andrew Scott <scottmeup@users.noreply.github.com>
2020-08-07 13:40:59 +1000

    Modify dataset3Params.m
    Passing test cases.

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -7,10 +7,6 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %   sigma based on a cross-validation set.
 %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -23,7 +19,36 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%list of values to use for C and sigma when trying to find the lowest error
+valueList = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+%initialise variables to hold the value of our lowesr cost, the C, and 
+%Sigma used to achieve that cost
+minCost = inf;
+C = 1;
+sigma = 1;
+
+%iterate over valueList to generate combinations of C and Sigma for
+%modelling & cost comparison
+[~, valueListDimension] = size(valueList);
+for thisC = 1:valueListDimension
+    for thisSigma = 1:valueListDimension
+        %generate cost for this particular combination of C and Sigma
+        thisModel = svmTrain(X, y, valueList(thisC), @(x1, x2) gaussianKernel(x1, x2, valueList(thisSigma)));
+        thisPrediction = svmPredict(thisModel, Xval);
+        thisCost = mean(double(thisPrediction ~= yval));
+        
+        %compare the cost of this iteration to lowest cost value previously
+        %calculated.
+        %store the values of thisCost, C, and Sigma if the cost for this 
+        %iteration is lower than any previously generated cost.
+        if thisCost < minCost
+            minCost = thisCost;
+            C = valueList(thisC);
+            sigma = valueList(thisSigma);
+        end
+    end
+end
 
 
 
